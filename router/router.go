b4ac9e21999ed02b68d2196eba8fe6ff8c51312f
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2023-03-02 17:15:09 +0700

    feat: adding CORS middleware

--- router/router.go
+++ router/router.go
@@ -11,16 +11,17 @@ func NewRouter(job controller.JobController, auth controller.AuthController, job
 	// gin.SetMode(config.GinMode())
 	router := gin.Default()
 	// router.Use(middleware.Logging())
+	router.Use(middleware.CORS())
 	router.Use(middleware.ErrorAppHandler())
 
 	api := router.Group("/api")
 	{
-		api.POST("/job", job.CreateJob)
+		api.POST("/job", middleware.JwtAuth(), job.CreateJob)
 		api.GET("/jobs", job.GetAllJob)
 		api.GET("/job/:job-id", job.GetJobDetail)
-		api.DELETE("/job/:job-id", job.DeleteJob)
-		api.PATCH("/job", job.UpdateJob)
-		api.PATCH("/job-categories", jobCategory.GetAllJobCategory)
+		api.DELETE("/job/:job-id", middleware.JwtAuth(), job.DeleteJob)
+		api.PATCH("/job", middleware.JwtAuth(), job.UpdateJob)
+		api.GET("/job-categories", jobCategory.GetAllJobCategory)
 		api.POST("/auth/login", auth.Login)
 	}
 	return router
